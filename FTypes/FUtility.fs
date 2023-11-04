342851af9ab93fc40b068f5602eb601b15212203
wvlab <wvlab@protonmail.com>
2023-11-04 13:58:29 +0200

    decouple Library.fs

--- FTypes/FUtility.fs
+++ FTypes/FUtility.fs
@@ -1,26 +1,5 @@
 ﻿namespace FunctionalRoguePound
 
-open Raylib_CsLo
-
-type Resolution =
-    | Windowed of width: int * height: int
-    | Borderless of width: int * height: int
-    | Fullscreen
-
-    member this.InitWindow(name: string) =
-        let aux (flags: Option<ConfigFlags>) (width: int) (height: int) (name: string) =
-            match flags with
-            | Some(f) -> Raylib.SetConfigFlags(f)
-            | None -> ()
-
-            Raylib.InitWindow(width, height, name)
-
-        match this with
-        | Windowed(x, y) -> aux None x y name
-        | Borderless(x, y) -> aux (Some ConfigFlags.FLAG_WINDOW_UNDECORATED) x y name
-        | Fullscreen -> aux (Some ConfigFlags.FLAG_FULLSCREEN_MODE) 0 0 name
-
-
 module FUtility =
     let BresenhamLine (x1: int) (y1: int) (x2: int) (y2: int) =
         let dx = abs (x2 - x1)
