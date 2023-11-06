cd7649dce4d21e801860a206515e9f2925788986
wvlab <wvlab@protonmail.com>
2023-11-06 17:04:25 +0200

    Dungeom.MainFrame: extract placing door methods

--- Dungeon.cs
+++ Dungeon.cs
@@ -7,71 +7,62 @@ namespace RoguePound;
 
 public readonly record struct Edge(Room Room1, Room Room2);
 
-public sealed record class DungeonMainFrame(Random Rand, ITile[,] Tiles)
+public sealed record class DungeonMainFrame(Random Rand, ITile[,] Tiles, List<Room> Rooms)
 {
     DumbTileSet DumbTileSet = new DumbTileSet();
 
-    public void PostProcTiles(ITileSet TileSet, List<Room> Rooms)
+    public void PostProcTiles(ITileSet TileSet)
     {
         foreach (Room room in Rooms)
         {
-            Tiles[room.x1 + Room.WallOffset, room.y1 + Room.WallOffset] = TileSet.LTCorner;
-            Tiles[room.x2 - Room.WallOffset, room.y1 + Room.WallOffset] = TileSet.RTCorner;
-            Tiles[room.x1 + Room.WallOffset, room.y2 - Room.WallOffset] = TileSet.LBCorner;
-            Tiles[room.x2 - Room.WallOffset, room.y2 - Room.WallOffset] = TileSet.RBCorner;
+            ChangeRoomTiles(TileSet, room);
+        }
+    }
 
-            foreach (int i in room.HWallXCoords())
+    private void ChangeRoomTiles(ITileSet TileSet, Room room)
+    {
+        Tiles[room.x1 + Room.WallOffset, room.y1 + Room.WallOffset] = TileSet.LTCorner;
+        Tiles[room.x2 - Room.WallOffset, room.y1 + Room.WallOffset] = TileSet.RTCorner;
+        Tiles[room.x1 + Room.WallOffset, room.y2 - Room.WallOffset] = TileSet.LBCorner;
+        Tiles[room.x2 - Room.WallOffset, room.y2 - Room.WallOffset] = TileSet.RBCorner;
+
+        foreach (int i in room.HWallXCoords())
+        {
+            HPlaceDoor(TileSet.TDoor, TileSet.HWall, i, room.y1 + Room.WallOffset);
+            HPlaceDoor(TileSet.BDoor, TileSet.HWall, i, room.y2 - Room.WallOffset);
+        }
+
+        foreach (int i in room.VWallYCoords())
+        {
+            VPlaceDoor(TileSet.LDoor, TileSet.VWall, room.x1 + Room.WallOffset, i);
+            VPlaceDoor(TileSet.RDoor, TileSet.VWall, room.x2 - Room.WallOffset, i);
+        }
+
+        foreach (int i in room.HWallXCoords())
+        {
+            foreach (int j in room.VWallYCoords())
             {
-                if (Tiles[i, room.y1 + Room.WallOffset + 1].Equals(DumbTileSet.Path)
-                && (Tiles[i, room.y1 + Room.WallOffset - 1].Equals(DumbTileSet.Path)))
-                {
-                    Tiles[i, room.y1 + Room.WallOffset] = TileSet.TDoor;
-                }
-                else
-                {
-                    Tiles[i, room.y1 + Room.WallOffset] = TileSet.HWall;
-                }
-
-                if (Tiles[i, room.y2 - Room.WallOffset + 1].Equals(DumbTileSet.Path)
-                && (Tiles[i, room.y2 - Room.WallOffset - 1].Equals(DumbTileSet.Path)))
-                {
-                    Tiles[i, room.y2 - Room.WallOffset] = TileSet.BDoor;
-                }
-                else
-                {
-                    Tiles[i, room.y2 - Room.WallOffset] = TileSet.HWall;
-                }
+                Tiles[i, j] = TileSet.Floor;
             }
+        }
+    }
 
-            foreach (int i in room.VWallYCoords())
-            {
-                if (Tiles[room.x1 + Room.WallOffset + 1, i].Equals(DumbTileSet.Path)
-                && (Tiles[room.x1 + Room.WallOffset - 1, i].Equals(DumbTileSet.Path)))
-                {
-                    Tiles[room.x1 + Room.WallOffset, i] = TileSet.LDoor;
-                }
-                else
-                {
-                    Tiles[room.x1 + Room.WallOffset, i] = TileSet.VWall;
-                }
+    private void VPlaceDoor(ITile door, ITile wall, int x, int y) =>
+        PlaceDoor(door, wall, x, y, x + 1, y, x - 1, y);
 
-                if (Tiles[room.x2 - Room.WallOffset + 1, i].Equals(DumbTileSet.Path)
-                && (Tiles[room.x2 - Room.WallOffset - 1, i].Equals(DumbTileSet.Path)))
-                {
-                    Tiles[room.x2 - Room.WallOffset, i] = TileSet.RDoor;
-                }
-                else
-                {
-                    Tiles[room.x2 - Room.WallOffset, i] = TileSet.VWall;
-                }
-            }
-            foreach (int i in room.HWallXCoords())
-            {
-                foreach (int j in room.VWallYCoords())
-                {
-                    Tiles[i, j] = TileSet.Floor;
-                }
-            }
+    private void HPlaceDoor(ITile door, ITile wall, int x, int y) =>
+        PlaceDoor(door, wall, x, y, x, y + 1, x, y - 1);
+
+    private void PlaceDoor(ITile door, ITile wall, int x, int y, int x1, int y1, int x2, int y2)
+    {
+        if (Tiles[x1, y1].Equals(DumbTileSet.Path)
+        && (Tiles[x2, y2].Equals(DumbTileSet.Path)))
+        {
+            Tiles[x, y] = door;
+        }
+        else
+        {
+            Tiles[x, y] = wall;
         }
     }
 }
@@ -369,14 +360,14 @@ public sealed class DungeonMaster
                 ResetTiles();
             }
         }
-        MainFrame.PostProcTiles(TileSet, Architect.Rooms);
+        MainFrame.PostProcTiles(TileSet);
     }
 
     public DungeonMaster(Action resetTiles, ITile[,] tiles) // Take reference to a player position?
     {
         Tiles = tiles;
         Architect = new DungeonArchitect(Rand, tiles);
-        MainFrame = new DungeonMainFrame(Rand, tiles);
+        MainFrame = new DungeonMainFrame(Rand, tiles, Architect.Rooms);
         ResetTiles = resetTiles;
     }
 }
